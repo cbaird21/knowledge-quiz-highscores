41e663adb276fef57e2f17957dacabce10d21565
unknown <chandler.baird21@gmail.com>
2022-10-31 20:16:25 -0600

    added highscore href, also edited how the inital info generates by button

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// timer that counts down from 60
+function countDown() {
+  var timeLeft = 60;
+  if (timeLeft > 1) {
+    timerEl.textContent = timeLeft + " seconds remaining";
+    timeLeft--;
+  } else if (timeLeft === 1) {
+    timerEl.textContent = timeLeft + "seconds remaining";
+    timeLeft--;
+  } else {
+    // Use `clearInterval()` to stop the timer
+    clearInterval(msgInterval);
+  }
+}
