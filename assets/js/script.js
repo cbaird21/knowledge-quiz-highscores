3248d1a44e80024364cd1b9d32dd4634e2913247
unknown <chandler.baird21@gmail.com>
2022-11-01 16:16:28 -0600

    updated js significantly and reduced html length due to converted it to js variables instead.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
-// timer that counts down from 60
+var questions = document.querySelector(".quiz");
+var timerEl = document.getElementById("seconds");
+var startBtn = document
+  .querySelector(".start_btn")
+  .addEventListener("click", runQuiz);
+// var timeLeft = countDown - now;
+
+var questions = [
+  {
+    title: "Commonly used data types Do not include.",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statment is encolsed with _____.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "curly brackets",
+  },
+  {
+    title:
+      "String values must be enclosed with _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes",
+  },
+];
+// console.log(questions[0].answer);
+
+function runQuiz() {
+  document.querySelector(".title").textContent = questions[0].title;
+}
+
 function countDown() {
-  var timeLeft = 60;
-  
+  var timerEl = 75;
   if (timeLeft > 1) {
     timerEl.textContent = timeLeft + " seconds remaining";
     timeLeft--;
